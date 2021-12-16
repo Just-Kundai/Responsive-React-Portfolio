4a6767c3d6076c156f567d9e610a9e0e0d92dc59
Just-Kundai <kundaikhuleya@outlook.com>
2021-12-17 03:51:43 +1100

    fixing errors

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import styled ThemeProvider from 'styled-components';
+import React from 'react'
+import styled, { keyframes, ThemeProvider } from 'styled-components'
 import {DarkTheme} from './Themes';
+
 
 import LogoComponent from '../subComponents/LogoComponent';
 import SocialIcons from '../subComponents/SocialIcons';
 import PowerButton from '../subComponents/PowerButton';
 import ParticleComponent from '../subComponents/ParticleComponent';
-import BigTitle from '../subComponents/BigTitle'
+import BigTitle from '../subComponents/BigTitle';
+
+
 
 
 const Box = styled.div`
