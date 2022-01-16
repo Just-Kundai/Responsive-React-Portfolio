a057050a081599847b6d0d24d6c32dbc2a839b07
Just-Kundai <kundaikhuleya@outlook.com>
2022-01-16 11:19:31 +1100

    added blogs

--- src/data/BlogData.js
+++ src/data/BlogData.js
@@ -7,6 +7,25 @@ export const Blogs = [
         imgSrc:"https://images.unsplash.com/photo-1573165662973-4ab3cf3d3508?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2069&q=80",
         link:"https://medium.com/@JustKundai/software-engineering-bootcamp-a-story-about-code-dreams-and-personal-development-1a4efed253e1"
     },
-    
+
+    {
+        id:2,
+        name:"Post-Bootcamp Blues",
+        tags:["codingbootcamp","softwareengineering","womenintech"],
+        date:"11 January, 2022",
+        imgSrc:"https://miro.medium.com/max/1400/0*oP129kP824gId042.jpg",
+        link:"https://medium.com/@JustKundai/post-bootcamp-blues-my-project-of-the-week-c0a81f163412"
+    },
+
+    {
+        id:3,
+        name:"Book Review: The God of Small Things",
+        tags:["book review","softwareengineering","womenintech"],
+        date:"13 January, 2022",
+        imgSrc:"https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1590282886i/9777.jpg",
+        link:"https://medium.com/@JustKundai/book-review-the-god-of-small-things-f46264890f88"
+    },
+
+
 
 ]
