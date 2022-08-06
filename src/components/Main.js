49a7df844e9457df795e0cd2cb5e549d2d33e2f9
Just-Kundai <kundaikhuleya@outlook.com>
2022-08-06 14:21:55 +1000

    adding meta tag to main

--- src/components/Main.js
+++ src/components/Main.js
@@ -128,6 +128,7 @@ const Main = () => {
     const handleClick = () => setClick(!click);
 
   return (
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <MainContainer>
      <DarkDiv click={click}/>
        <Container>
