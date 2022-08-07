c6cce766ebc958bbb337f00516d36f85f436fcd9
Just-Kundai <kundaikhuleya@outlook.com>
2022-08-07 19:26:22 +1000

    adding css to logo component

--- src/subComponents/LogoComponent.js
+++ src/subComponents/LogoComponent.js
@@ -5,14 +5,14 @@ import { DarkTheme } from '../components/Themes'
 
 
 
-const Logo = styled.h1`
+const Logo = styled.h2`
 display: inline-block;
 color: ${props => props.color === 'dark' ? DarkTheme.text : DarkTheme.body};
 font-family: 'Pacifico',cursive;
 position: fixed;
 left: 2rem;
 top: 2rem;
-z-index:10;
+z-index:3;
 `
 
 const LogoComponent = (props) => {
