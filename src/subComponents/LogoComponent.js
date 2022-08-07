18344bf6970fe6853bded0c2af5c0d344fafe24a
Just-Kundai <kundaikhuleya@outlook.com>
2022-08-07 19:10:51 +1000

    adding css to logo component

--- src/subComponents/LogoComponent.js
+++ src/subComponents/LogoComponent.js
@@ -9,7 +9,7 @@ const Logo = styled.h1`
 display: inline-block;
 color: ${props => props.color === 'dark' ? DarkTheme.text : DarkTheme.body};
 font-family: 'Pacifico',cursive;
-position: fixed;
+position: flex;
 left: 2rem;
 top: 2rem;
 z-index:3;
@@ -17,7 +17,7 @@ z-index:3;
 
 const LogoComponent = (props) => {
     return (
-        <Logo color={props.theme} display=flex >
+        <Logo color={props.theme}>
           <div>
           Just Kundai
           </div>
