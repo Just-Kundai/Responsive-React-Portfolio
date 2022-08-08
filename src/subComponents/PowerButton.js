cc711cf5ec0b855158eb31d41cf8ee57d0c24889
Just-Kundai <kundaikhuleya@outlook.com>
2022-08-08 16:05:02 +1000

    editing for mobile

--- src/subComponents/PowerButton.js
+++ src/subComponents/PowerButton.js
@@ -27,15 +27,7 @@ z-index:3;
 
 cursor: pointer;
 
-&:hover{
-    background-color: rgba(0,255,0,0.4);
-    box-shadow: 0 0 8px 6px rgba(0,255,0,0.2);
-}
 
-&>*:first-child{
-    text-decoration: none;
-    color: inherit;
-}
 `
 
 const PowerButton = () => {
