132554fde5b621f033206e1edbf489ed49bc62d6
Just-Kundai <kundaikhuleya@outlook.com>
2022-08-08 16:30:20 +1000

    editing for mobile

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -14,7 +14,7 @@ import BigTitle from '../subComponents/BigTitle';
 
 const Box = styled.div`
 background-color: ${props => props.theme.body};
-width: 100vw;
+width: 50vw;
 height: 100vh;
 position: relative;
 overflow: auto;
