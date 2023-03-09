6289d794064d233bd588ebb36206acc439084002
Just-Kundai <kundaikhuleya@outlook.com>
2023-03-09 23:37:50 +1100

    updating portfolio

--- src/data/BlogData.js
+++ src/data/BlogData.js
@@ -8,7 +8,14 @@ export const Blogs = [
         link:"https://medium.com/@JustKundai/software-engineering-bootcamp-a-story-about-code-dreams-and-personal-development-1a4efed253e1"
     },
 
-  
+    {
+        id:2,
+        name:"Software Engineering Bootcamp: A story about code, dreams and personal development",
+        tags:["codingbootcamp","softwareengineering"],
+        date:"13 December, 2021",
+        imgSrc:"https://images.unsplash.com/photo-1573165662973-4ab3cf3d3508?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2069&q=80",
+        link:"https://medium.com/@JustKundai/software-engineering-bootcamp-a-story-about-code-dreams-and-personal-development-1a4efed253e1"
+    },
 
 
 
